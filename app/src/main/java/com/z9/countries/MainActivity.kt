1feedc8be4edae71c16d19845153f239e04939d7
Tautvydas Jurgaitis <tautvydas.jurgaitis@gmail.com>
2024-10-27 01:27:15 +0300

    Fix

--- app/src/main/java/com/z9/countries/MainActivity.kt
+++ app/src/main/java/com/z9/countries/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import com.z9.countries.presentation.CountriesScreen
 import com.z9.countries.ui.theme.CountriesTheme
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
